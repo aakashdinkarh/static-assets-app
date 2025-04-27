822ca25b3e7dadc9de3bab219c662cfa2a0b5d99
Aakash Dinkar <aakashdinkarh@gmail.com>
2025-04-27 21:37:47 +0530

    caching utils

---
+++ src/utils/cachingUtils.ts
@@ -0,0 +1,29 @@
+import { CACHE_EXPIRATION } from 'constants/github';
+
+import { cache } from 'constants/github';
+import type { RepoItem } from 'types/github';
+
+export const getCachedData = (path: string): RepoItem[] | null => {
+  const cacheEntry = cache[path];
+  if (!cacheEntry) return null;
+
+  const now = Date.now();
+  if (now - cacheEntry.timestamp > CACHE_EXPIRATION) {
+    // Cache expired, remove it
+    delete cache[path];
+    return null;
+  }
+
+  return cacheEntry.data;
+};
+
+export const setCachedData = (path: string, data: RepoItem[]) => {
+  cache[path] = {
+    data,
+    timestamp: Date.now(),
+  };
+};
+
+export const deleteCachedData = (path: string) => {
+  delete cache[path];
+};
