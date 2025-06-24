ad71eac93aabe66a3e4bd33cc21c82760f982b6a
Aakash Dinkar <aakashdinkarh@gmail.com>
2025-06-24 08:55:00 +0530

    persist user info with localStorage & cookie vaidation

--- src/utils/apiHandlers.util.ts
+++ src/utils/apiHandlers.util.ts
@@ -1,7 +1,10 @@
+import { deleteCookie, AUTHORIZATION_COOKIE_NAME } from 'utils/cookie.util';
+
 export const checkError = async (response: Response) => {
   if (response.ok) return;
 
   if (response.status === 401) {
+    deleteCookie(AUTHORIZATION_COOKIE_NAME);
     throw new Error('User not logged in or session expired');
   }
 
