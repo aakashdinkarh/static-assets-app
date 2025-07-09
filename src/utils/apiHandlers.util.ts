025ba5fc64d8173de3758c6ee5edaf5520bd7cf4
Aakash Dinkar <aakashdinkarh@gmail.com>
2025-07-10 02:06:49 +0530

    logout user when 401, from UI as well

--- src/utils/apiHandlers.util.ts
+++ src/utils/apiHandlers.util.ts
@@ -1,10 +1,18 @@
+import { STORAGE_KEYS } from 'constants/storage.constant';
 import { deleteCookie, AUTHORIZATION_COOKIE_NAME } from 'utils/cookie.util';
+import { removeFromLocalStorage } from 'utils/storage.util';
 
 export const checkError = async (response: Response) => {
   if (response.ok) return;
 
   if (response.status === 401) {
     deleteCookie(AUTHORIZATION_COOKIE_NAME);
+    removeFromLocalStorage(STORAGE_KEYS.USER_INFO);
+
+    import('store/GithubUserInfoStore')
+      .then(module => module.useGithubUserInfoStore())
+      .then(({ setUserInfo }) => setUserInfo(null));
+
     throw new Error('User not logged in or session expired');
   }
 
